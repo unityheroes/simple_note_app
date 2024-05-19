f0e42855972665f27eee108dc75dc76e5af493cd
Ahmed Elhabak <ahmed.elhabak101@gmail.com>
2024-05-19 22:38:04 +0300

    add cubit and notestates

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,16 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+sealed class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+final class AddNodeLoading extends AddNoteState {}
+
+final class AddNodeSuccess extends AddNoteState {}
+
+final class AddNodeFailure extends AddNoteState {
+  final String errorMessage;
+
+  AddNodeFailure({required this.errorMessage});
+}
