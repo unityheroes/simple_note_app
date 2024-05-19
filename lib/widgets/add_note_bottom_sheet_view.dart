22777a43b93a74ba495a47b1cacf81952bc2d24b
Ahmed Elhabak <ahmed.elhabak101@gmail.com>
2024-05-19 19:14:07 +0300

    finish modalbottomsheet notebottom sheet

--- lib/widgets/add_note_bottom_sheet_view.dart
+++ lib/widgets/add_note_bottom_sheet_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_note_app/widgets/custom_button.dart';
 import 'package:simple_note_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -6,17 +7,26 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomTextField(
-          hintText: "Title Note",
-          maxlines: 1,
-        ),
-        CustomTextField(
-          hintText: "Content",
-          maxlines: 4,
-        )
-      ],
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          CustomTextField(
+            hintText: "Title Note",
+            maxlines: 1,
+          ),
+          CustomTextField(
+            hintText: "Content",
+            maxlines: 4,
+          ),
+          SizedBox(
+            height: 64,
+          ),
+          CustomButton(),
+          SizedBox(
+            height: 64,
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:simple_note_app/appsetting.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: GestureDetector(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: kPrimaryColor.withAlpha(80),
+              gradient: LinearGradient(colors: [
+                Colors.white.withAlpha(16),
+                Colors.white.withAlpha(32),
+                Colors.white.withAlpha(64),
+                Colors.white.withAlpha(128),
+              ])),
+          height: 64,
+          child: const Center(
+            child: Text(
+              "Add",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
